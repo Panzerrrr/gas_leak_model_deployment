d633c21207bad8bbef68092251c08b370d914319
Nidal.Simplon <nidalzaiani@gmail.com>
2023-01-05 09:15:49 +0100

    st.beta_columns replace by st.columns

--- app/app.py
+++ app/app.py
@@ -100,7 +100,7 @@ if datafile is not None:
 
     image = Image.open(datafile)
 
-    col1, col2, col3 = st.beta_columns([1,6,1])
+    col1, col2, col3 = st.columns([1,6,1])
 
     with col1:
         st.write("")
